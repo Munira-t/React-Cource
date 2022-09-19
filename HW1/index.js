2ba3c50670c6e4ec1b7af9d167ab93ac2d072776
munira-t <munira.ati@gmail.com>
2022-09-19 15:11:06 +0300

    add HW-1

---
+++ HW1/index.js
@@ -0,0 +1,62 @@
+
+let container = document.getElementById("root")
+const element = ( 
+
+<div className="container">
+
+
+<section className="header">
+  <img src="https://i.pinimg.com/564x/88/9f/bc/889fbcbc30a1935a62c45d0848dac10c.jpg"className="img"></img>
+  <h1 className="text-red ff-playfair-display fs-600 text-center title-name">Munira.</h1>
+  <h3 className="text-red ff-poppins fs-200 mt-5 text-center fade-right title-name">I'm designer, thinker, devloper</h3>
+  </section>
+
+
+<section className="skills">
+<di className="row gap-1">
+  <div className="col">
+    <h3 className="ff-playfair-display fs-400 title">Skills</h3>
+    </div>
+  <div className="col">
+    
+  </div>
+</di>
+<div className="mt-5 pt-5 mb-5 skills-content">
+  <div className="row gap-1">
+    <div className="col html"><a href="https://www.w3schools.com/html/html_intro.asp">HTML</a></div>
+    <div className="col css">CSS</div>
+    <div className="col sass">SASS</div>
+  </div>
+  
+  <div className="row gap-1 mt-1">
+    <div className="col js">JavaScript</div>
+    <div className="col bootstrap">Bootstrap</div>
+    <div className="col react">Soon React!</div>
+  </div>
+  
+  <div className="row gap-1 mt-1">
+    <div className="col figma">Figma</div>
+    <div className="col trello">Trello</div>
+    <div className="col zeplin">Zeplin</div>
+  </div>
+</div>
+</section>
+<h4></h4>
+  </div>
+);
+
+ReactDOM.render(element, container);
+
+  // <ul style={{}}>
+  //   <li>HTML</li>
+  //   <li>CSS</li>
+  //   <li>SASS</li>
+  //   <li>JS</li>
+  //   <li>Bootstrap</li>
+  //   <li>Soon to crash it React!</li>
+  //   <li></li>
+  //   <li>Prototyping</li>
+  //   <li>Figma</li>
+  //   <li>Trello</li>
+  //   <li>Zeplin</li>
+  // </ul>
